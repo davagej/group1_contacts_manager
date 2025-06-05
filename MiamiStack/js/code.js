40525fad968070bf4353b682349b6cf2c3b40111
makerJulia <136854669+makerJulia@users.noreply.github.com>
2025-06-04 22:47:57 -0400

    Update code.js

--- MiamiStack/js/code.js
+++ MiamiStack/js/code.js
@@ -289,7 +289,7 @@ function searchColor()
 let modal = document.getElementById("myModal");
 
 // Get the button that opens the modal
-vlet btn = document.getElementById("myBtn");
+let btn = document.getElementById("myBtn");
 
 // Get the <span> element that closes the modal
 let span = document.getElementsByClassName("close")[0];
