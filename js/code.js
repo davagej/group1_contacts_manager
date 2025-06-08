cdadeb26f7c0ae360bb55cd88e002e6fc4167173
makerJulia <136854669+makerJulia@users.noreply.github.com>
2025-06-08 11:03:18 -0400

    Update code.js

--- js/code.js
+++ js/code.js
@@ -33,12 +33,12 @@ function goToContactsPage()
 	window.location.href = "Miami_ContactsPage.html"
 }
 
-function confirmDelete("+current.ID+")
+function confirmDelete(ID)
 {
 	let result = confirm ("<b>WARNING<b>\nYou are about to delete a contact, which is a permanent action that cannot be undone.\n<b>Do you want to delete this contact?<b>");
 	if (result == TRUE)
 	{
-		deleteContact("+current.ID+");
+		deleteContact("ID");
 	}
 }
 
